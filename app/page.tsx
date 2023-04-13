2dbf701076a86a067c5b0bab3a986a6649e5ab52
attTuo <tuomaalanatte@gmail.com>
2023-04-13 13:40:38 +0300

    banner added to frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,30 @@ export default async function HomePage(){
 
     return(
         <div>
-            <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+            <div className="container mx-auto px-5 py-2 lg:px-32 lg:py-4">
+                <Image 
+                    className="block h-full w-full rounded-lg object-cover object-center"
+                    src="/../public/images/cupsBanner.jpg" 
+                    alt="cupBanner" 
+                    width={1000} height={1000}
+                />
+            </div>
+            <div className="container mx-auto px-5 py-2 lg:px-32 lg:pb-4 pt-0">
                 <div className="-m-1 flex flex-wrap md:-m-2">
                     <div className="flex w-1/2 flex-wrap">
                         <div className="w-1/2 p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cups1.jpg" 
-                                alt="cupPic" 
-                                width={300} height={250}
+                                alt="cups1" 
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-1/2 p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cups2.jpg"
-                                alt="cupPicture2" 
+                                alt="cups2" 
                                 width={300} height={250}
                             />
                         </div>
@@ -27,8 +35,8 @@ export default async function HomePage(){
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cup1.jpg"
-                                alt="dragon"
-                                width={300} height={250}
+                                alt="cup1"
+                                width={1000} height={1000}
                             />
                         </div>
                     </div>
@@ -37,24 +45,24 @@ export default async function HomePage(){
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cup2.jpg"
-                                alt="drawing1"
-                                width={300} height={250}
+                                alt="cup2"
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-1/2 p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cups3.jpg"
-                                alt="drawing2"
-                                width={300} height={250}
+                                alt="cup3"
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-1/2 p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cups4.jpg"
-                                alt="cups1"
-                                width={300} height={250}
+                                alt="cups4"
+                                width={1000} height={1000}
                             />
                         </div>
                     </div>
@@ -63,24 +71,24 @@ export default async function HomePage(){
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cups5.jpg"
-                                alt="drawing2"
-                                width={300} height={250}
+                                alt="cups5"
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-1/2 p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cups6.jpg"
-                                alt="cups1"
-                                width={300} height={250}
+                                alt="cups6"
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-full p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/bunny2.jpg"
-                                alt="drawing1" 
-                                width={300} height={250}
+                                alt="bunny2" 
+                                width={1000} height={1000}
                             />
                         </div>
                     </div>
@@ -89,24 +97,24 @@ export default async function HomePage(){
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/cupsStack.jpg"
-                                alt="drawing2"
-                                width={300} height={250}
+                                alt="cupsStack"
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-1/2 p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="/../public/images/cupsPair.jpg"
-                                alt="cups1"
-                                width={300} height={250}
+                                src="/../public/images/bunnyPins.jpg"
+                                alt="bunnyPins"
+                                width={1000} height={1000}
                             />
                         </div>
                         <div className="w-full p-1 md:p-2">
                             <Image 
                                 className="block h-full w-full rounded-lg object-cover object-center"
                                 src="/../public/images/dog.jpg"
-                                alt="drawing1"
-                                width={300} height={250}
+                                alt="dog"
+                                width={1000} height={1000}
                             />
                         </div>
                     </div>
--- ui/footer.tsx
+++ ui/footer.tsx
@@ -2,7 +2,7 @@ export default function FooterComponent(){
 
     return(
         <div>
-            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content rounded-lg">
                 <div>
                     <p>Copyright © 2023 - Design by Tiikasalo</p>
                 </div>
